4f5515493c725828eefb681944f03aab7e5659cb
Fanora Ssh Prem <hesaceza476@gmail.com>
2025-01-19 22:24:37 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -298,7 +298,7 @@ clear
 echo -e "${BIBlue}╭══════════════════════════════════════════╮${NC}"
 echo -e "${BIBlue}│ ${BGCOLOR}          DOWNLOAD EXTRA MENU           ${NC}${BIBlue} │${NC}"
 echo -e "${BIBlue}╰══════════════════════════════════════════╯${NC}"
-wget https://raw.githubusercontent.com/FanoraSsh/autoscriptssh/main/menu/update.sh && chmod +x update.sh && ./update.sh
+wget https://raw.githubusercontent.com/FanoraSsh/autoscriptssh/main/update.sh && chmod +x update.sh && ./update.sh
 clear
 
 echo -e "${BIBlue}╭══════════════════════════════════════════╮${NC}"
